66bd2d8567b8a8d8bf3cb773589a5ee32a01bf9a
Rafael Araujo <rafa123839@gmail.com>
2022-11-29 20:26:11 -0300

    tentando por localstorage

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
 import { Alert, FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Empty from "../../components/Empty";
 import Separator from "../../components/Separator";
+import { loadTask } from "../../libs/storage";
 import Task from "../../components/Task";
 import { styles } from "./styles";
 
@@ -12,20 +14,53 @@ interface Task {
 
 export default function Home() {
     const [todoList, setTodoList] = useState<string[]>([])
-    const [completedTasks, setCompletedTasks] = useState<string[]>([])
+    const [newTask, setNewTask] = useState<Task[]>([])
     const [task, setTask] = useState("")
     const [inputSelected, setInputSelected] = useState(false)
 
+    useEffect(() => {
+        // loadTask()
+        
+        // async function loadTask2() {
+        //     const tasks = await loadTask()
+        //     setNewTask(tasks)
+        // }
+        // loadTask2()
+    }, []);
+
+
+
+    async function addTaskStorage() {
+        try {
+            await AsyncStorage.setItem("@tasks:task", JSON.stringify(newTask));
+            console.log('salvou')
+        } catch (error) {
+            Alert.alert("Deu Error")
+            throw new Error("Deu erro")
+        }
+    }
+
+    async function addCheckStorage() {
+        try {
+            AsyncStorage.setItem("@tasks:taskCheckd", JSON.stringify(todoList));
+            console.log('salvou')
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Deu Error")
+        }
+    }
+
     function handleTaskAdd() {
-        if(task === ''){
-            return Alert.alert("Tarefa vazia", "Tarefa não pode ser vazia.") 
+        if (task === '') {
+            return Alert.alert("Tarefa vazia", "Tarefa não pode ser vazia.")
         }
 
-        if (todoList.includes(task)) {
-            return Alert.alert("Tarefa já Existe", "Já existe uma tarefa na lista com esse nome.")
+        const Task: Task = {
+            title: task,
+            done: false
         }
-
-        setTodoList(precState => [...precState, task])
+        setNewTask(precState => [...precState, Task])
+        // addTaskStorage()
         setTask('')
     }
 
@@ -34,8 +69,7 @@ export default function Home() {
             {
                 text: 'Sim',
                 onPress: () => {
-                    setTodoList(prevState => prevState.filter(participant => participant !== name))
-                    setCompletedTasks(prevState => prevState.filter(title => title !== name))
+                    setNewTask(prevState => prevState.filter(task => task.title !== name))
                 }
             },
             {
@@ -46,11 +80,19 @@ export default function Home() {
     }
 
     function handleMarkTaskAsDone(checked: Task) {
-        if (checked.done === true) {
-            setCompletedTasks(precState => [...precState, checked.title])
-        } else {
-            setCompletedTasks(prevState => prevState.filter(title => title !== checked.title))
-        }
+        setNewTask(array => array.map(item => {
+            if (item.title === checked.title) {
+                if (checked.done === true) {
+                    item.done = true;
+                } else {
+                    console.log(item.done)
+                    item.done = false;
+                }
+            }
+            return item
+        }))
+        console.log(newTask)
+
     }
 
     return (
@@ -77,29 +119,29 @@ export default function Home() {
             <View style={styles.containerTasks}>
                 <View style={styles.create}>
                     <Text style={styles.textCreate}>Criadas</Text>
-                    <Text style={styles.textCount}>{todoList.length}</Text>
+                    <Text style={styles.textCount}>{newTask.length || 0}</Text>
                 </View>
                 <View style={styles.create}>
                     <Text style={styles.textEnd}>Concluidas</Text>
-                    <Text style={styles.textCount}>{completedTasks.length}</Text>
+                    <Text style={styles.textCount}>{newTask.filter(task => task.done === true).length || 0}</Text>
                 </View>
             </View>
 
-            
+
             {
                 todoList.length > 0 ? <></> : <Separator />
             }
 
-            <View style={{flex: 1}}>
+            <View style={{ flex: 1 }}>
                 <View style={{ marginHorizontal: 24 }}>
                     <FlatList
-                        data={todoList}
-                        keyExtractor={item => item}
+                        data={newTask}
+                        keyExtractor={item => item.title}
                         renderItem={({ item }) => (
                             <Task
-                                key={item}
-                                taskTitle={item}
-                                onRemove={() => handleRemoveTask(item)}
+                                key={item.title}
+                                taskTitle={item.title}
+                                onRemove={() => handleRemoveTask(item.title)}
                                 onPress={handleMarkTaskAsDone}
                             />
                         )}
@@ -108,6 +150,7 @@ export default function Home() {
                             <Empty />
                         )}
                     />
+
                 </View>
 
             </View>
